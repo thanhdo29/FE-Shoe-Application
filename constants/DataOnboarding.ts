81b6b3d6f7e10218dfd186228c9664e2ca52e0fd
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-09-12 14:17:19 +0700

    Create data for Onboarding

---
+++ constants/DataOnboarding.ts
@@ -0,0 +1,30 @@
+interface DataOnboarding {
+    des: string
+    id: string
+    img: string
+    title: string
+  }
+  
+  const dataOnboarding: DataOnboarding[] = [
+    {
+      des: 'Smart, Gorgeous & Fashionable Collection',
+      id: '1',
+      img: require('~/assets/images/shoes1.png'),
+      title: 'Start Journey With Nike'
+    },
+    {
+      des: 'There Are Many Beautiful And Attractive Plants To Your Room',
+      id: '2',
+      img: require('~/assets/images/shoes2.png'),
+      title: 'Follow Latest Style Shoes'
+    },
+    {
+      des: 'Amet Minim Lit Nodeseru Saku Nandu sit Alique Dolor',
+      id: '3',
+      img: require('~/assets/images/shoes3.png'),
+      title: 'Summer Shoes Nike 2022'
+    }
+  ]
+  
+  export default dataOnboarding
+  
