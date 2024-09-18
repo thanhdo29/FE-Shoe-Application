32a95f96ab031638b0b6a4879f661e87dce36ac2
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-09-18 21:42:44 +0700

    Implement HomeTemplate

--- components/templates/HomeTemplate.tsx
+++ components/templates/HomeTemplate.tsx
@@ -1,14 +1,176 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Entypo, Feather, Ionicons } from '@expo/vector-icons'
+import { MotiView } from 'moti'
+import React, { useState } from 'react'
+import { FlatList, SafeAreaView, ScrollView, StyleSheet, Text, useColorScheme, View } from 'react-native'
 
-const HomeTemplate = () => {
+import FormInputWithLabel from '~/components/atoms/FormInputWithLabel'
+import getColors from '~/constants/Colors'
+import dataBranch from '~/constants/DataBranch'
+import useTranslation from '~/hooks/useTranslation'
+import { ButtonRenderBranch } from '../atoms/ButtonRenderBranch'
+import Header from '../molecules/Header'
+import { ShoesCategory } from '../molecules/ShoesCategory'
+import { ListShoesItem } from '../organisms/ListShoeItem'
+import { BannerShoesItem } from '../molecules/BannerShoesItem'
+
+const HomeTemplate: React.FC = () => {
+  const color = getColors(useColorScheme())
+
+  const [selectedBranch, setSelectBranch] = useState<{
+    logo: number
+    name: string
+  }>()
+  const { t } = useTranslation()
+
+  const leftIconOfHeader = (
+    <Entypo name="grid" size={25} color={color.midnightBlue} />
+  )
+  const rightIconOfHeader = (
+    <Ionicons name="bag-handle-outline" size={25} color={color.midnightBlue} />
+  )
+
   return (
-    <View>
-      <Text>HomeTemplate</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Header
+          leftIcon={leftIconOfHeader}
+          rightIcon={rightIconOfHeader}
+
+        />
+
+        <View style={{ marginBottom: 32, marginTop: 30 }}>
+          <FormInputWithLabel
+            iconLeft={
+              <Feather name="search" size={23} color={color.slateGray} />
+            }
+          />
+        </View>
+
+        <FlatList
+          data={dataBranch}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item, index }) => {
+            const isSelected = selectedBranch?.name === item.name
+            const transitionStyle = isSelected
+              ? [styles.selectItem, { backgroundColor: color.cornflowerBlue }]
+              : styles.unSelectItem
+            return (
+              <MotiView
+                from={{ opacity: 0, translateY: 50 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                transition={{ delay: index * 200 }}
+                style={transitionStyle}
+              >
+                <ButtonRenderBranch
+                  icon={item.logo}
+                  onPress={() => {
+                    if (isSelected) {
+                      setSelectBranch(undefined)
+                    } else {
+                      setSelectBranch(item)
+                    }
+                  }}
+                  isSelected={isSelected}
+                />
+                {isSelected && (
+                  <MotiView
+                    from={{
+                      opacity: 0,
+                      translateX: -50
+                    }}
+                    animate={{
+                      opacity: 1,
+                      translateX: 0
+                    }}
+                    transition={{ duration: 350, type: 'timing' }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        fontWeight: 500,
+                        color: color.white,
+                        marginLeft: 8
+                      }}
+                    >
+                      {item.name}
+                    </Text>
+                  </MotiView>
+                )}
+              </MotiView>
+            )
+          }}
+          keyExtractor={(item) => item.name}
+        />
+
+        <ShoesCategory
+          leftText={t('home.popularShoes')}
+          rightText={t('home.seeAll')}
+          containerStyle={{
+            marginBottom: 16,
+            marginTop: 24
+          }}
+          leftTextStyle={{
+            fontSize: 16
+          }}
+        />
+
+        <ListShoesItem dataShoes={dataBranch} />
+
+        <ShoesCategory
+          leftText={t('home.newArrivals')}
+          rightText={t('home.seeAll')}
+          containerStyle={{
+            marginBottom: 16,
+            marginTop: 24
+          }}
+          leftTextStyle={{
+            fontSize: 16
+          }}
+        />
+
+        <BannerShoesItem
+          label={t('bestChoice')}
+          nameShoes="Nike Air Jordan"
+          price="849.69"
+        />
+
+        <ShoesCategory
+          leftText={t('home.topSeller')}
+          rightText={t('home.seeAll')}
+          containerStyle={{
+            marginBottom: 16,
+            marginTop: 24
+          }}
+          leftTextStyle={{
+            fontSize: 16
+          }}
+
+        />
+        <ListShoesItem dataShoes={dataBranch} />
+      </View>
+    </ScrollView>
   )
 }
 
-export default HomeTemplate
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingBottom: 130,
+    paddingTop: 20
+  },
+  selectItem: {
+    alignItems: 'center',
+    borderRadius: 40,
+    flexDirection: 'row',
+    marginRight: 16,
+    padding: 8,
 
-const styles = StyleSheet.create({})
+  },
+  unSelectItem: {
+    marginRight: 16
+  }
+})
+
+export default HomeTemplate
