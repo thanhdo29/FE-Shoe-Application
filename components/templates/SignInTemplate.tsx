0f964f3143efd9b46e04596e4cc34857fd1e07b9
Truong <truongtdph40209@fpt.edu.vn>
2024-10-07 22:10:45 +0700

    Add api authentication

--- components/templates/SignInTemplate.tsx
+++ components/templates/SignInTemplate.tsx
@@ -8,6 +8,17 @@ import useTranslation from '~/hooks/useTranslation'
 import Header from '~/components/molecules/Header'
 import InputForm from '~/components/molecules/InputForm'
 import Footer from '~/components/molecules/Footer'
+import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { API_URL } from '~/API/ipconfig'
+
+
+// const API_URL = "http://192.168.1.11:3000/"
+
+const isValidEmail = (email: string): boolean => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return emailRegex.test(email)
+}
 
 const SignInTemplate: React.FC = (): JSX.Element => {
   const [email, setEmail] = useState<string>('')
@@ -32,10 +43,44 @@ const SignInTemplate: React.FC = (): JSX.Element => {
 
   }
 
-  const ButtonSignin = (): void => {
-    router.replace('/(tabs)/home')
-    
-  }
+
+
+  const handleLogin = async (): Promise<void> => {
+    // Kiểm tra thông tin đầu vào
+    if (!isValidEmail(email) || !password) {
+      alert(t('login.invalidCredentials')); // Hiển thị thông báo lỗi
+      return;
+    }
+  
+    // Tạo payload để gửi lên server
+    const payload = {
+      email: email,
+      password: password,
+    };
+  
+    try {
+      // Gửi yêu cầu POST tới API đăng nhập
+      const response = await axios.post(`${API_URL}signin`, payload);
+  
+      // Xử lý phản hồi thành công
+      if (response.status === 200) {
+        const { token, user } = response.data;
+        alert(t('login.success')); // Hiển thị thông báo thành công
+  
+        // Lưu token vào AsyncStorage
+        await AsyncStorage.setItem('authToken', token);
+  
+        // Chuyển hướng sau khi đăng nhập thành công
+        router.replace('/(tabs)/home'); // hoặc trang mong muốn
+      }
+    } catch (error) {
+      // Xử lý lỗi khi gửi yêu cầu hoặc phản hồi từ server
+      alert("Đăng nhập thất bại")
+      console.error('Đăng nhập thất bại:', error);
+    }
+  };
+  
+  
   return (
     <SafeAreaView style={{
       ...styles.container,
@@ -56,7 +101,7 @@ const SignInTemplate: React.FC = (): JSX.Element => {
         visibleFormInputWithLabel={false}
         onChangeEmailText={setEmail}
         onChangePasswordText={setPassword}
-        onLoginPress={ButtonSignin}
+        onLoginPress={handleLogin}
         onRecoveryPasswordPress={redirectToForgot}
         buttonTitle={t('signIn.signIn')}
         googleButtonTitle={t('signIn.signInWithGoogle')}/>
